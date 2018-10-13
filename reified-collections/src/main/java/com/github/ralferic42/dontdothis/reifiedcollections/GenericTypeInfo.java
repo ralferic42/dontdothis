e116acc6b8e748dd583d3f9a5f4a3645bb82feb6
ralferic42 <43819941+ralferic42@users.noreply.github.com>
2018-10-13 12:54:20 +0200

    no separate scanner class needed: former scanner is now GenericTypeInfo

--- reified-collections/src/main/java/com/github/ralferic42/dontdothis/reifiedcollections/GenericTypeInfo.java
+++ reified-collections/src/main/java/com/github/ralferic42/dontdothis/reifiedcollections/GenericTypeInfo.java
@@ -3,20 +3,29 @@ package com.github.ralferic42.dontdothis.reifiedcollections;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
- * Scanner for generic type arguments, usable only after construction of an instance of an anonymous class
+ * Scanner/Result-Container for generic type arguments, usable only after construction of an instance of an
+ * anonymous class
  */
 public class GenericTypeInfo {
 
-  public List<Class<?>> scanForGenericTypes(Object createdInstance) {
-    List<Class<?>> resultList = new ArrayList<>();
+  private final ParameterizedType parameterizedType;
+  private final List<Class<?>> rawTypes;
+
+  public GenericTypeInfo(Object createdInstance) {
     // scan for actual type arguments...
     // ...type information is accessible via the generic superclass
     Type genericSuperclass = createdInstance.getClass().getGenericSuperclass();
-    if (genericSuperclass instanceof ParameterizedType) {
-      ParameterizedType parameterizedType = (ParameterizedType) genericSuperclass;
+    if (!(genericSuperclass instanceof ParameterizedType)) {
+      this.parameterizedType = null;
+      this.rawTypes = Collections.emptyList();
+    }
+    else {
+      this.parameterizedType = (ParameterizedType) genericSuperclass;
+      List<Class<?>> resultList = new ArrayList<>();
       Type[] actualTypeArguments = parameterizedType.getActualTypeArguments();
       for (Type t : actualTypeArguments) {
         if (t instanceof ParameterizedType) {
@@ -26,8 +35,9 @@ public class GenericTypeInfo {
           resultList.add((Class<?>) t);
         }
       }
+      this.rawTypes = Collections.unmodifiableList(resultList);
     }
-    return resultList;
+
   }
 
 }
